68eaf9709470e87b81c6fd057bf3244c3ea8ff1c
Neskj <pronin.qq@gmail.com>
2024-05-21 16:19:38 +0300

    first commit

---
+++ TestHibernate/src/main/java/neskj/TestHibernate/TestHibernateApplication.java
@@ -0,0 +1,13 @@
+package neskj.TestHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHibernateApplication.class, args);
+	}
+
+}
---
+++ TestHibernate/src/test/java/neskj/TestHibernate/TestHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package neskj.TestHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
